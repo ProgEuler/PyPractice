f13ecae364e4c68401d9ecc317019db11e12e943
ProgEuler <sarufkhan568@gmail.com>
2024-01-06 15:27:49 +0600

    update

--- Tip-Calculator/main.py
+++ Tip-Calculator/main.py
@@ -1,6 +1,6 @@
 try:
     food_amount = float(input("Enter your food amount $: "))
-    tip_percentage = float(input("Enter tip percentage: ")) / 100
+    tip_percentage = float(input("Enter tip percentage: %")) / 100
 
     tip_amount = food_amount * tip_percentage
     total = food_amount + tip_amount
